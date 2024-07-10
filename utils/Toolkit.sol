85da949f737eea1ea730f55ef6db46ba06334f39
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-07-10 14:47:16 -0400

    Improved getAddress

--- utils/Toolkit.sol
+++ utils/Toolkit.sol
@@ -124,20 +124,30 @@ contract Toolkit {
     }
 
     function getAddress(string memory key) public view returns (address) {
-        require(addressMap[key] != address(0), string.concat("address not found: ", key));
-        return addressMap[key];
-    }
-
-    function getAddress(string calldata name, uint256 chainid) public view returns (address) {
-        if (chainid == ChainId.All) {
-            return getAddress(name);
+        // search for current block.chainid format
+        string memory localKey = string.concat(chainIdToName[block.chainid].lower(), ".", key);
+        if (addressMap[localKey] != address(0)) {
+            return addressMap[localKey];
         }
-        string memory key = string.concat(chainIdToName[chainid].lower(), ".", name);
-        return getAddress(key);
+
+        // search for explicit <chain_name>.key format first
+        if (addressMap[key] != address(0)) {
+            return addressMap[key];
+        }
+
+        revert(string.concat("address not found: ", key));
     }
 
-    function getAddress(uint256 chainid, string calldata name) public view returns (address) {
-        return getAddress(name, chainid);
+    function getAddress(uint256 chainid, string memory key) public view returns (address) {
+        if (chainid != ChainId.All) {
+            key = string.concat(chainIdToName[chainid].lower(), ".", key);
+        }
+
+        if (addressMap[key] != address(0)) {
+            return addressMap[key];
+        }
+
+        revert(string.concat("address not found: ", key));
     }
 
     function getPairCodeHash(string calldata key) public view returns (bytes32) {
